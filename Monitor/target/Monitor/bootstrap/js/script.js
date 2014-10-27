5f14b4d4b906536864f48dde31da2a571241f182
calvinhamus <calvinhamus@u.boisestate.edu>
2014-10-26 21:11:05 -0600

    initial

---
+++ Monitor/target/Monitor/bootstrap/js/script.js
@@ -0,0 +1,91 @@
+var BASE_URL = "http://cs597-calvin-loadbalancer:80/socialnetwork/chatter/";
+//var BASE_URL = "http://localhost:8080/socialnetwork/chatter/";
+onload = function() {
+    	var url = document.URL;
+		getProcessingTime();
+		getQueueDepth();
+};
+function getErrors(data)
+{
+	try{
+
+$.ajax({
+  url: BASE_URL + 'monitor/errors/'+data ,
+  context: document.body
+}).done(function(data) {
+var headers=[ ""], rows={}
+     headers.push("Errors");
+   $.each(data.processTimeList,function(clientIdx,item){
+  
+	   rows[clientIdx]=item;
+
+  })
+  var rowHtml='<tr><th>'+headers.join('</th><th>')+'</th></tr>';
+  $.each(rows,function(key, arr){
+    rowHtml+='<tr><td>'+key+'</td>';
+    rowHtml +='<td>'+arr+'  Milliseconds'+'</td><td>'+'</td></tr>';
+	$('#processTable').html(rowHtml);
+  })
+});
+ 
+    }catch(err){
+    			alert(err);
+    }
+}
+function getProcessingTime()
+{
+	try{
+
+$.ajax({
+  url: BASE_URL + 'monitor/processingtime' ,
+  context: document.body
+}).done(function(data) {
+var headers=[ ""], rows={}
+     headers.push("Processing Time");
+   $.each(data.processTimeList,function(clientIdx,item){
+  
+	   rows[clientIdx]=item;
+
+  })
+  var rowHtml='<tr><th>'+headers.join('</th><th>')+'</th></tr>';
+  $.each(rows,function(key, arr){
+    rowHtml+='<tr><td>'+key+'</td>';
+    rowHtml +='<td>'+arr+'  Milliseconds'+'</td><td>'+'</td></tr>';
+	$('#processTable').html(rowHtml);
+  })
+})
+.fail(function(data) {
+    alert( data );
+  })
+ 
+    }catch(err){
+    			alert(err);
+    }
+}
+function getQueueDepth()
+{
+	try{
+
+$.ajax({
+  url: BASE_URL + 'monitor/queuedepth' ,
+  context: document.body
+}).done(function(data) {
+var headers=[ ""], rows={}
+     headers.push("Pending Requests");
+//   $.each(data.processTimeList,function(clientIdx,item){
+  
+	//   rows[clientIdx]=item;
+
+//  })
+  var rowHtml='<tr><th>'+headers.join('</th><th>')+'</th></tr>';
+ // $.each(rows,function(key, arr){
+  //  rowHtml+='<tr><td>'+1+'</td>';
+    rowHtml +='<td>'+data+'  Pending'+'</td><td>'+'</td></tr>';
+	$('#pendingTable').html(rowHtml);
+  //})
+});
+ 
+    }catch(err){
+    			alert(err);
+    }
+}
